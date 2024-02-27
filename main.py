3ef4bd1f33089b163223537f7f4ad26d051b3a89
leonardogamateixeira <leoteixeirastack@outlook.com>
2024-02-26 23:43:51 -0300

    Create(main.py): Criei o inicio da API

---
+++ main.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
+
+precos = {
+    1: {
+        'titulo': 'Cintaralho',
+        'preco': 200,
+        'vendido': True
+    }
+}
+
+class Item(BaseModel):
+    titulo: str
+    preco: int
+    vendido: bool
+
+@app.get('/')
+def index():
+    return {"hello": "world"}
+
+@app.get('/items/{item_id}', response_model=Item)
+def pega_posicao(item_id: int):
+    return precos[item_id]
