1a1bd805750b4daf06a48315111e1d5710e36af3
leonardogamateixeira <leoteixeirastack@outlook.com>
2024-02-27 23:43:29 -0300

    bla

--- main.py
+++ main.py
@@ -3,23 +3,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-precos = {
-    1: {
-        'titulo': 'Cintaralho',
-        'preco': 200,
-        'vendido': True
-    }
-}
+precos = {1: {"titulo": "Cintaralho", "preco": 200, "vendido": True}}
+
 
 class Item(BaseModel):
     titulo: str
     preco: int
     vendido: bool
 
-@app.get('/')
+
+@app.get("/")
 def index():
     return {"hello": "world"}
 
-@app.get('/items/{item_id}', response_model=Item)
+
+@app.get("/items/{item_id}", response_model=Item)
 def pega_posicao(item_id: int):
     return precos[item_id]
